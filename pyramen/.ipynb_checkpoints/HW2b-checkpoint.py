f51cdc15e209469ddb6460013e5ca50b58b621ca
dbrims <dbrims@gmail.com>
2020-03-26 15:13:20 -0400

    First pass at homework

---
+++ pyramen/.ipynb_checkpoints/HW2b-checkpoint.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 09:21:53 2020
+
+@author: dbrims
+"""
+
+import csv
+
+#initialize two lists to accept CSV data
+menu=[]
+
+#open CSV file and read in data
+with open('menu_data.csv') as data
+    file_data = csv.reader(f, delimiter=',')
+    
---
+++ pyramen/HW2b.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 09:21:53 2020
+
+@author: dbrims
+"""
+
+import csv
+
+#pull data from menu file and create a nested loop of the data
+
+menu=[]
+
+with open('./data/menu_data.csv') as data:
+    file_data = csv.reader(data, delimiter=',')
+    next(file_data)
+    for row in file_data:
+        menu.append(row)
+        
+    
+sales=[]
+with open('./data/sales_data.csv') as data:
+    file_data = csv.reader(data, delimiter=',')
+    next(file_data)
+    for row in file_data:
+        for item in row:
+            sales.append(item)           
+
+          
+report={}
+metrics=['01-count','02-revenue','03-cogs','04-profit']
+
+    
+sales_item=[]
+sales_quantity=[] 
+sales_len=len(sales) 
+
+
+for i in range(3, sales_len, 5):
+    sales_quantity.append(int(sales[i]))
+for i in range(4,sales_len, 5):
+    sales_item.append(sales[i])
+
+for item in sales_item:
+    if item not in report:
+        report[item]={}
+        for m in metrics:
+            report[item][m]=0
+
+         
+menu_item=[]
+cost=[]
+price=[]
+for r in menu:
+    for item in range(len(r)):
+        if item==0:
+            menu_item.append(r[item])
+
+        elif item==3:
+            price.append(float(r[item]))
+    
+        elif item==4:
+            cost.append(float(r[item]))           
+            
+len_items=len(menu_item)
+len_sales_item=len(sales_item)
+
+for x in range(len_sales_item):
+    match=False
+    for y in range(len_items):
+        if menu_item[y]==sales_item[x]:
+            profit=price[y]-cost[y]
+            report[sales_item[x]]["01-count"] += sales_quantity[x]
+            report[sales_item[x]]["02-revenue"] += price[y] * sales_quantity[x]
+            report[sales_item[x]]["03-cogs"] += cost[y] * sales_quantity[x]
+            report[sales_item[x]]["04-profit"] += profit * sales_quantity[x]
+            match=True
+    if match==False:
+        print(f"{sales_item} does not equal {menu_item}! NO MATCH!")
+        
+
+f=open('pyramen1.txt','w')       
+for key in report:
+    f.write(str(key)+' '+str(report[key])+'\n')   
+f.close()
+ 
+    
+
+            
+
+            
+           
+    
+            
+
+            
+
+        
+
+        
+    
+
+    
+    
+
